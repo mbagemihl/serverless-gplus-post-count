e276e116be34d024211dc059be4549d92a055ba8
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2018-02-26 17:49:50 +0100

    Remove imports as well

--- src/main/kotlin/serverless_poc/Application.kt
+++ src/main/kotlin/serverless_poc/Application.kt
@@ -1,8 +1,5 @@
 package serverless_poc
 
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
-
 class Application {
     fun hello(name: String) = "Hello World!"
 }
