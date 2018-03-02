50a700cc7b3ced411ad51116fa2b4dd5490014c6
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2018-03-02 16:12:24 +0100

    Remove unneeded dependency again

--- src/main/kotlin/serverless_poc/Application.kt
+++ src/main/kotlin/serverless_poc/Application.kt
@@ -1,8 +1,5 @@
 package serverless_poc
 
-import com.amazonaws.services.lambda.runtime.Context
-import com.amazonaws.services.lambda.runtime.RequestHandler
-
-class Application : RequestHandler<String,String> {
-    override fun handleRequest(name: String, context: Context) = "Hello World!"
+class Application {
+    fun handleRequest(name: String) = "Hello World!"
 }
