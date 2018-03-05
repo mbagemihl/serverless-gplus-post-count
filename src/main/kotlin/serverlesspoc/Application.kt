3e7d37af7ac15fbc05d1cef7b716c43438997c37
Dominik Pabst <Pablo1@gmx.de>
2018-03-05 21:14:01 +0100

    Update Application.kt

--- src/main/kotlin/serverlesspoc/Application.kt
+++ src/main/kotlin/serverlesspoc/Application.kt
@@ -1,5 +1,16 @@
 package serverlesspoc
 
+import java.io.*
+import com.fasterxml.jackson.module.kotlin.*
+
+data class HandlerInput(val who: String)
+data class HandlerOutput(val message: String)
+
 class Application {
-    fun handleRequest(name: String) = "Hello World!"
+    val mapper = jacksonObjectMapper()
+
+    fun handler(input: InputStream, output: OutputStream): Unit {
+        val inputObj = mapper.readValue<HandlerInput>(input)
+        mapper.writeValue(output, HandlerOutput("Hello ${inputObj.who}"))
+    }
 }
