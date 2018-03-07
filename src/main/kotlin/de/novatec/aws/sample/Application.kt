e39fc0dc14db66edadbfd3e29696eb6deb17d02d
Pabst Dominik <dominik.pabst@novatec-gmbh.de>
2018-03-07 17:07:59 +0100

    Extends Handler

--- src/main/kotlin/de/novatec/aws/sample/Application.kt
+++ src/main/kotlin/de/novatec/aws/sample/Application.kt
@@ -1,6 +1,19 @@
 package de.novatec.aws.sample
 
-class Application {
-    fun handler(name: String) = "Hello Peter!"
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import java.io.InputStream
+import java.io.OutputStream
 
+
+data class HandlerInput(val who: String)
+data class HandlerOutput(val message: String)
+
+class Application {
+    val mapper = jacksonObjectMapper()
+
+    fun handler(input: InputStream, output: OutputStream): Unit {
+        val inputObj = mapper.readValue<HandlerInput>(input)
+        mapper.writeValue(output, HandlerOutput("Hello ${inputObj.who}"))
+    }
 }
