cb8e735996944b9132d78ab792202c774f754241
MBA <marcel.bagemihl@novatec-gmbh.de>
2018-04-27 12:57:07 +0200

    first try to reach google+ api

--- src/main/kotlin/de/novatec/aws/sample/Application.kt
+++ src/main/kotlin/de/novatec/aws/sample/Application.kt
@@ -1,6 +1,29 @@
 package de.novatec.aws.sample
 
-class Application {
-    fun handler(name: String) = "Hello Peter!"
+import com.google.gson.Gson
+import khttp.get
+import java.text.SimpleDateFormat
+import java.util.*
 
+class Application {
+    fun handler() = getGooglePlus()
+
+    val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
+    fun getGooglePlus():String {
+        val asd = get("https://www.googleapis.com/plus/v1/people/+novatecgmbhdeutschland/activities/public?key=")
+        val result:Result = Gson().fromJson(asd.text,Result::class.java)
+        return result.items.filter { item -> formatter.parse(item.published).after(Date(2018,1,1))}.size.toString()
+    }
+}
+
+data class Action (
+    val id: String,
+    val verb: String,
+    val published: String
+)
+
+data class Result (
+        val items: List<Action>
+) {
+    constructor(items: Array<Action>) : this(items.toList())
 }
